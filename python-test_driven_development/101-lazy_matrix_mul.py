4221d93cd2fcd5208452a894265fe57779e3922f
Diego Landony Lima Toledo <132611526+Diego-Landony@users.noreply.github.com>
2023-10-10 16:25:54 -0600

    I say Hello to you

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
-"""
-Multiplies 2 matrices by using 'numpy' module.
-"""
-import numpy as lazy
+"""Defines a matrix multiplication function using NumPy."""
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """Return the multiplication of two matrices.
+
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
     """
-    Function to multiplies 2 matrices.
-    m_a: As a list of lists.
-    m_b: As a list of lists.
-    Return: The resulting matrix.
-    """
-    return lazy.matmul(m_a, m_b)
+
+    return (np.matmul(m_a, m_b))
--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""This module contain a function that prints a square"""
+"""Defines a square-printing function."""
+
 
 def print_square(size):
-    """print_square function prints a square
+    """Print a square with the # character.
 
     Args:
-        size (int): size of the square
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
-    if type(size) != int or (type(size) != int and size < 0):
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(0, size):
-        j = 0
-        for j in range(0, size):
-            print("#", end="")
-        print()
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
