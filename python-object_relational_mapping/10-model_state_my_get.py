2a143a89413d24f5b15d42d193060b8406f0c8bb
Diego Landony Lima Toledo <132611526+Diego-Landony@users.noreply.github.com>
2023-12-02 19:23:57 -0600

    1

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""
-prints the State object with the name passed as argument from
-the database hbtn_0e_6_usa
-"""
-
+"""List all states"""
 from sys import argv
 from model_state import Base, State
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(engine)
-    state = session.query(State).filter(State.name == argv[4]).first()
-    try:
-        print("{}".format(state.id))
-    except:
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).\
+        filter(State.name == argv[4]).order_by(State.id).all()
+    if states:
+        print("{}".format(states[0].id))
+    else:
         print("Not found")
     session.close()
