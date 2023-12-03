9d18251b63ea700cd61194e5c5bbd75fa554c48e
Diego Landony Lima Toledo <132611526+Diego-Landony@users.noreply.github.com>
2023-12-02 19:25:56 -0600

    1

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,17 @@
-x#!/usr/bin/python3
-"""
-lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa, safe from injection
-"""
+#!/usr/bin/python3
+"""Lists states"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name=%s\
-                    ORDER BY states.id ASC", (argv[4],))
-    rows = cursor.fetchall()
-    for state in rows:
-        print(state)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+                (argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
